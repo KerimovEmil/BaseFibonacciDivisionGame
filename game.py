ccea00fe662c203de7e430477ab4709ce6405cb3
KerimovEmil <emilkerimov93@gmail.com>
2022-11-13 19:43:36 -0500

    adding highlight of correct rows

--- game.py
+++ game.py
@@ -41,6 +41,16 @@ class Game:
             if self.fib_prod[x] == '1':
                 pygame.draw.circle(self.screen, 'red', (x_pos + BLOCK_SIZE / 2, last_row_y - BLOCK_SIZE / 2), 0.6 * BLOCK_SIZE / 2)
 
+        # highlight rows based on self.fib_div
+        print(f'dividend={self.div}, fib_dividend={self.fib_div}')
+
+        for y, y_pos in enumerate(range(START_Y, end_y, BLOCK_SIZE)):
+            if self.fib_div[y] == '1':
+                # rect = pygame.Rect(START_X, y_pos, end_x, BLOCK_SIZE)
+                rect = pygame.Rect(START_X, y_pos, grid_width * BLOCK_SIZE, BLOCK_SIZE)
+                pygame.draw.rect(self.screen, 'green', rect, 5)
+
+
     def build_initial_game_grid(self):
         self.screen.fill('white')
         self.drawGrid(grid_width=self.grid_width, grid_height=self.grid_height)
